5b22c8292f382f8697893b724b298e58736d6460
rnveach <rveach02@gmail.com>
2016-11-13 17:48:22 -0500

    regression

---
+++ checkstyle-regression/src/main/java/com/github/checkstyle/Utils.java
@@ -0,0 +1,73 @@
+package com.github.checkstyle;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class Utils {
+    public static String getWorkingDirectory() throws IOException {
+        final File directory = new File(new File(".").getCanonicalPath());
+
+        return directory.getCanonicalPath();
+    }
+
+    public static String getTesterDirectory() throws IOException {
+        return new File("../checkstyle-tester").getCanonicalPath();
+    }
+
+    public static String getTesterSrcDirectory() throws IOException {
+        return new File("../checkstyle-tester/src/main/java").getCanonicalPath();
+    }
+
+    public static String getTesterDownloadsDirectory() throws IOException {
+        return new File("../checkstyle-tester/downloads").getCanonicalPath();
+    }
+
+    public static void deleteFolderContents(File folder) {
+        File[] files = folder.listFiles();
+
+        if (files != null) {
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    deleteFolderContents(f);
+
+                    f.delete();
+                }
+                else {
+                    f.delete();
+                }
+            }
+        }
+
+    }
+
+    public static void copyFolderContents(Path source, Path destination) throws IOException {
+        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(final Path dir,
+                    final BasicFileAttributes attrs) throws IOException {
+                if (!dir.getFileName().toString().equals(".git")) {
+                    Files.createDirectories(destination.resolve(source.relativize(dir)));
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                return FileVisitResult.SKIP_SUBTREE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs)
+                    throws IOException {
+                if (file.getFileName().toString().endsWith(".java")) {
+                    Files.copy(file, destination.resolve(source.relativize(file)));
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+}
